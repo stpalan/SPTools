8b2b1ab79ad9266575c850acdf57b77d30c50ce0
Stefan Palan <stefan.palan@uni-graz.at>
2020-01-27 11:45:25 +0100

    Changed infix from %+% to %§%.
    Bugfixes.

--- R/InfixPaste.R
+++ R/InfixPaste.R
@@ -1,4 +1,4 @@
-#' %+%
+#' %§%
 #'
 #' Infix operator to combine strings. Equivalent to paste(..., sep="").
 #' @param x An object of type character.
@@ -7,7 +7,7 @@
 #' @export
 #' @examples
 #' z<-5
-#' "The result is: " %+% z %+% "."
+#' "The result is: " %§% z %§% "."
 #' > The result is 5.
 
-"%+%" = function(x,y) paste(x,y,sep="")
+"%§%" = function(x,y) paste(x,y,sep="")
