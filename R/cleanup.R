fbb2f3398213e31fedf5945411ae186f9c2921f7
Stefan Palan <stefan.palan@uni-graz.at>
2020-12-02 23:35:26 +0100

    Added vector pattern input to cleanup.

--- R/cleanup.R
+++ R/cleanup.R
@@ -1,16 +1,21 @@
 #' cleanup
 #'
 #' Function which cleans up the GlobalEnv by removing all objects whose name commences with "t."
-#' @param pattern A character vector containing the RegEx pattern of the object names to remove. Make sure to surround names by "^...$" if you want to match the name exactly.
+#' @param pattern A character vector containing the RegEx patterns of the object names to remove. Make sure to surround names by "^...$" if you want to match the name exactly.
 #' @keywords rm
 #' @export
 #' @examples
 #' Instead of writing:
-#' rm(list=ls(pattern="^t."))
+#' rm(list=ls(pattern="^t[.]"))
 #' one can write
 #' cleanup()
+#'
+#' You can also expressly specify objects to clean up:
+#' cleanup(c("^temp_var$","^x$"))
 
 cleanup<-function(pattern="^t[.]"){
-    objs <- ls(pos = ".GlobalEnv")
-    rm(list = objs[grep(pattern, objs)], pos = ".GlobalEnv")
+    for(p in pattern){
+        objs <- ls(pos = ".GlobalEnv")
+        rm(list = objs[grep(p, objs)], pos = ".GlobalEnv")
+    }
 }
